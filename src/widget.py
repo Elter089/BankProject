52fdc923056b7a98cea222644a876d9c7b13dd3b
Viktor Palyanitsa <elter089@gmail.com>
2024-12-04 18:03:17 +0300

    Добавил новый модуль widjet и создал в нем функции

---
+++ src/widget.py
@@ -0,0 +1,38 @@
+from src.masks import
+
+
+def mask_account_card(card_or_account_number: str) -> str:
+    """Функция, которая принимает номер карты или счета в банке"""
+    number = card_or_account_number.split()
+    if len(number) > 2:
+        return "Ошибка: неверный ввод"
+
+    ident = " ".join(number[:-1])
+    new_number = number[-1]
+
+    if ident.lower().startswith("счет"):
+        masked_number = get_mask_account(new_number)
+        return f"{ident} **{masked_number}"
+    else:
+        masked_number = get_mask_card_number(new_number)
+        return f"{ident} {masked_number}"
+
+
+def get_date(date_string: str) -> str:
+    """
+    Функция, которая принимает на вход строку с датой в формате:
+    "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате
+    "ДД.ММ.ГГГГ" ("11.03.2024").
+    """
+    date = date_string.split("T")[0]
+    day, month, year = date.split("-")
+    return f"{day}.{month}.{year}"
+
+
+user_input = input("Введите номер карты или счета: ")
+masked_output = mask_account_card(user_input)
+print(masked_output)
+
+
+print(get_date("2024-03-11T02:26:18.671407"))
